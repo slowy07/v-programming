ab415c09575361ef460176d80496a93a1be24ef4
slowy07 <slowy.arfy@gmail.com>
2022-04-17 05:35:38 +0700

    bubble sort testing
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ bubblesortTest.v
@@ -0,0 +1,18 @@
+fn bubblesort(mut array []int) {
+	for i := 0; i < array.len - 1; i++ {
+		for j := 0; j < array.len - i - 1; j++ {
+			if array[j] > array[j + 1] {
+				array[j], array[j + 1] = array[j + 1], array[j]
+			}
+		}
+	}
+}
+
+mut arr := [64, 22, 32, 21, 82, 11]
+println("not sorted test array: $arr")
+
+bubblesort(mut arr)
+println("sorted array")
+for i in arr {
+	println(i)
+}
